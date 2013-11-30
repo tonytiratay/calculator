a87ff8ab51ee973fed93950d5b67c392c08d6944
Tony Tiratay <tony.tiratay@gmail.com>
2013-11-30 04:52:08 +0100

    Best design and ratio Time spent / hourly rate added

--- js/calculator.js
+++ js/calculator.js
@@ -1,27 +1,42 @@
 $(document).ready(function() {
-	var tva = 19.6
-    var ht = 0
-    var ttc = 0
+
+	var tva = 19.6;
+	var valtauxhoraire = 40;
+	var valchargescoop = 10;
+    var ht = 100;
+    var ttc = 0;
+
+    
+    // Récupération options
 
     $("#valtva").on("keyup change", function() {
   	  tva = this.value; 
-      });
+      })
 
-    $("#valauxhoraire").on("keyup change", function() {
-  	  tva = this.value; 
-      });
+    $("#valtauxhoraire").on("keyup change", function() {
+      $(".resultat").text("Soit " + (ht / valtauxhoraire) + " heures travaillées pour " + valtauxhoraire + "€ de l'heure.");
+  	  valtauxhoraire = this.value;
+      })
+
+    $("#valchargescoop").on("keyup change", function() {
+  	  valchargescoop = this.value; 
+      })
+
+    // Calculs Formulaire
 
     $("#valht").on("keyup change", function() {
   	  ht = this.value; 
       ttc = ht * (1 + (tva / 100));
       $("#valttc").val(ttc.toFixed(2));
-      });
+      $(".resultat").text("Soit " + (ht / valtauxhoraire) + " heures travaillées pour " + valtauxhoraire + "€ de l'heure.");
+      })
 
     $("#valttc").on("keyup change", function() {
   	  ttc = this.value; 
       ht = ttc - ttc * (tva / 100);
       $("#valht").val(ht.toFixed(2));
-      });
+      $(".resultat").text("Soit " + (ht / valtauxhoraire) + " heures travaillées pour " + valtauxhoraire + "€ de l'heure.");
+      })
 
 
 
